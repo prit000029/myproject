4f94287d9da7da1335fc2c0ac667987417cbef9a
pritam pal <prit000029@gmail.com>
2019-11-12 10:08:54 +0000

    Intial Comit

---
+++ calc.py
@@ -0,0 +1,7 @@
+echo "# myproject" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin https://github.com/prit000029/myproject.git
+git push -u origin master
+
