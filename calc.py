957c5784b7e4f45ce6128b32ccc2a957e80d46de
pritam pal <prit000029@gmail.com>
2019-11-12 10:18:56 +0000

    Modifide multipy function

--- calc.py
+++ calc.py
@@ -2,6 +2,6 @@ echo "# myproject" >> README.md
 git init
 git add README.md
 git commit -m "first commit"
-git remote add origin https://github.com/prit000029/myproject.git
+git remote add origin/github.com/prit000029/myproject.git
 git push -u origin master
 
